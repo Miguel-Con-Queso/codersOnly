0224b119748fbde69d7c9d5f5770b1eec5a8b953
lest210 <lest210@gmail.com>
2021-09-20 17:59:11 -0700

    uncommented server package

--- server/server.js
+++ server/server.js
@@ -27,15 +27,14 @@ server.start().then(() => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+ //serve up static assets
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, '../client/build')));
+ }
 
-// serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+ app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+ });
 
 db.once('open', () => {
   app.listen(PORT, () => {
