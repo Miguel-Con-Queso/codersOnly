d44db50fc7c66bcd1c707720cf28c09c5c6a5267
Nina Davis <nina.k.davis2@gmail>
2021-09-21 13:20:15 -0500

    update

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 // serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.use(express.static('public'));
+
 }
 
 app.get('*', (req, res) => {
