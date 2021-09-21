4f0355103b09b458a02743eca54fdbf4d2475c12
MiguelConQueso <michael_pnkstn@yahoo.com>
2021-09-21 11:52:00 -0400

    uncommented server

--- server/server.js
+++ server/server.js
@@ -1,43 +1,43 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({
+const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 // integrate our Apollo server with the Express application as middleware
 server.start().then(() => {
-  console.log('server started')
-  server.applyMiddleware({ app});
+  console.log("server started");
+  server.applyMiddleware({ app });
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
